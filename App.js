87554e7b36a1ed74ff6fbb2e6782c5b82d5b0eac
ocfranz <franzjoseo@gmail.com>
2020-06-26 03:14:08 -0500

    wip(): basic bottom tab navigator

--- App.js
+++ App.js
@@ -21,6 +21,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './src/screens/Home';
+import LoginScreen from './src/screens/Login';
+import UserScreen from './src/screens/User';
+import FiltersScreen from './src/screens/Filters';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -40,10 +43,13 @@ const App = () => {
 	}
 
 	return (
+		<NavigationContainer>
 		<Tab.Navigator>
+		<Tab.Screen name="User" component={UserScreen} />
 			<Tab.Screen name="Home" component={HomeScreen} />
-			<Tab.Screen name="Settings" component={LoginScreen} />
+			<Tab.Screen name="Filters" component={FiltersScreen} />
 		</Tab.Navigator>
+		</NavigationContainer>
 	);
 };
 
